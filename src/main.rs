d95ecfb6d47e993159896919d479352ea7dc27b2
Tạ Quang Khôi <khoi0941@gmail.com>
2024-10-20 16:38:10 +0700

    Add Position Component

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,12 @@ use bevy::prelude::*;
 fn main() {
     App::new().run();
 }
+
+/**
+ * Components: Rust structs that implement the Component trait
+ */
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
