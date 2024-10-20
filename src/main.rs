c059e974ba5455f8316e739f6537f282c41c88fa
Tạ Quang Khôi <khoi0941@gmail.com>
2024-10-20 16:43:29 +0700

    Mein First Components

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_systems(Startup, add_people)
         .add_systems(Update, hello_world)
         .run();
 }
@@ -11,6 +12,18 @@ fn hello_world() {
     println!("hello world!");
 }
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn((Person, Name("Đỗ Quyên".to_string())));
+}
 
 /**
  * Components: Rust structs that implement the Component trait
